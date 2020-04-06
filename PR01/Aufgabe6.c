8fe6cd87d3dd82129ce1038078b996bb42aa3947
CodePhisch <codephisch@gmail.com>
2020-04-06 22:40:50 +0200

    Optimized task 1.6

--- PR01/Aufgabe6.c
+++ PR01/Aufgabe6.c
@@ -6,45 +6,8 @@ int ascii2int(char *s)
 {
     int zahl = 0;
 
-    // Startpointer des Strings kopieren damit dieser erhalten bleibt
-    char *c = s;   
-
-    // Tatsächliche Länge der Zahl ermitteln
-    int zlength = 0;
-    while(c < s + strlen(s)) 
-    {
-        if(*c - '0' <= 9 && *c - '0' >= 0)
-        {
-            zlength++;
-        } else
-        {
-            break;
-        }
-        
-        c++;
-    }
-
-    // Pointer auf den Anfang des Strings zurücksetzen
-    c = s;
-
-    // Variablen für Berechnung der aktuellen Zehnerpotenz
-    int exp, pow;
-
-    // Für alle Zeichen der tatsächlichen Zahl durchgehen
-    while(c < s + zlength) 
-    {
-        // Aktuelle Stelle der Zahl ermitteln (Exponent der Zehnerpotenz)
-        exp = zlength - (c - s) - 1;
-        
-        // Zehnerpotenz an aktueller Position ermitteln
-        pow = 1;
-        for(int i = 0; i < exp; i++)
-        {
-            pow *= 10;
-        }
-
-        zahl += pow * (*c - '0');
-        c++;
+    for(int i = 0; s[i] <= '9' && s[i] >= '0'; i++) {
+        zahl = zahl * 10 + (s[i] - '0');
     }
     
     return zahl;
